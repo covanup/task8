bc9b17f7a9493aa0114e6c59687314b21b385ab0
Vladimir Khramtsov <khramtsov.git@gmail.com>
2023-05-24 19:12:09 +0300

    Task 8

--- Task1/View+Constaints.swift
+++ Task1/View+Constaints.swift
@@ -17,8 +17,8 @@ extension UIView {
         let constaints: [NSLayoutConstraint?] = [
             top.flatMap { topAnchor.constraint(equalTo: superview!.topAnchor, constant: $0) },
             leading.flatMap { leadingAnchor.constraint(equalTo: superview!.leadingAnchor, constant: $0) },
-            bottom.flatMap { bottomAnchor.constraint(equalTo: superview!.bottomAnchor, constant: $0) },
-            trailing.flatMap { trailingAnchor.constraint(equalTo: superview!.trailingAnchor, constant: $0) },
+            bottom.flatMap { bottomAnchor.constraint(equalTo: superview!.bottomAnchor, constant: -$0) },
+            trailing.flatMap { trailingAnchor.constraint(equalTo: superview!.trailingAnchor, constant: -$0) },
         ]
         NSLayoutConstraint.activate(constaints.compactMap { $0 })
         return self
@@ -30,8 +30,8 @@ extension UIView {
         let constaints: [NSLayoutConstraint?] = [
             top.flatMap { topAnchor.constraint(equalTo: superview!.layoutMarginsGuide.topAnchor, constant: $0) },
             leading.flatMap { leadingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.leadingAnchor, constant: $0) },
-            bottom.flatMap { bottomAnchor.constraint(equalTo: superview!.layoutMarginsGuide.bottomAnchor, constant: $0) },
-            trailing.flatMap { trailingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.trailingAnchor, constant: $0) },
+            bottom.flatMap { bottomAnchor.constraint(equalTo: superview!.layoutMarginsGuide.bottomAnchor, constant: -$0) },
+            trailing.flatMap { trailingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.trailingAnchor, constant: -$0) },
         ]
         NSLayoutConstraint.activate(constaints.compactMap { $0 })
         return self
@@ -43,8 +43,8 @@ extension UIView {
         let constaints: [NSLayoutConstraint?] = [
             top.flatMap { topAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.topAnchor, constant: $0) },
             leading.flatMap { leadingAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.leadingAnchor, constant: $0) },
-            bottom.flatMap { bottomAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.bottomAnchor, constant: $0) },
-            trailing.flatMap { trailingAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.trailingAnchor, constant: $0) },
+            bottom.flatMap { bottomAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.bottomAnchor, constant: -$0) },
+            trailing.flatMap { trailingAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.trailingAnchor, constant: -$0) },
         ]
         NSLayoutConstraint.activate(constaints.compactMap { $0 })
         return self
@@ -61,5 +61,17 @@ extension UIView {
         NSLayoutConstraint.activate(constaints.compactMap { $0 })
         return self
     }
+    
+    @discardableResult
+    func addSize(width: CGFloat? = nil, height: CGFloat? = nil) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        let constaints: [NSLayoutConstraint?] = [
+            width.flatMap { widthAnchor.constraint(equalToConstant: $0) },
+            height.flatMap { heightAnchor.constraint(equalToConstant: $0) },
+        ]
+        NSLayoutConstraint.activate(constaints.compactMap { $0 })
+        return self
+    }
+    
 }
 
--- Task1/ViewController.swift
+++ Task1/ViewController.swift
@@ -1,4 +1,48 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
+    
+    private let side: CGFloat = 36.0
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(
+            image: .init(
+                systemName: "person.crop.circle.fill",
+                withConfiguration: UIImage.SymbolConfiguration(pointSize: side, weight: .regular, scale: .medium)
+            )
+        )
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(scrollView)
+        navigationItem.title = "Avatar"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            if let navigationBar = navigationController?.navigationBar,
+               let navigationBarLargeTitleViewClass = NSClassFromString("_UINavigationBarLargeTitleView"),
+               let navigationBarLargeTitleView = navigationBar.subviews.first(where: { $0.isKind(of: navigationBarLargeTitleViewClass.self) }),
+               imageView.superview == nil {
+                imageView
+                    .addToView(navigationBarLargeTitleView)
+                    .addConstraints(bottom: 10, trailing: 12)
+                    .addSize(width: side, height: side)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        scrollView.contentSize = view.bounds.size
+        scrollView.contentSize.height *= 2
+    }
 }
